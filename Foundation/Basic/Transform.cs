7595fa06ac42b78c08575c635862a1a8cb02a73e
Sean Lenard B. <cordysean@163.com>
2026-01-08 16:34:05 +0800

    feat: local and global shenanigans

---
+++ Foundation/Basic/Transform.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdofaiTheater.Foundation.Basic
+{
+    public class Transform
+    {
+        public Transform(Transform parent)
+        {
+            Parent = parent;
+        }
+
+
+
+        public Vector2 LocalTranslation { get; set; } = new();
+        public Matrix2 LocalTransformation { get; set; } = new();
+
+        public Transform? Parent { get; private set; } = null;
+
+
+
+        // WARN(seanlb): I have zero confidence in these two things. May not work as intended!
+        public Vector2 GlobalTranslation()
+        {
+            if (Parent is null) { return LocalTranslation; }
+            return Parent.GlobalTransformation() * Parent.GlobalTranslation() + LocalTranslation;
+        }
+        
+        public Matrix2 GlobalTransformation()
+        {
+            if (Parent is null) { return LocalTransformation; }
+            return Parent.GlobalTransformation() * LocalTransformation;
+        }
+    }
+}
