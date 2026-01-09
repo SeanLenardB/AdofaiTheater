4b1f4a4ebc2a55dc824a29aec70e82fbcdd332f2
SeanLenardB <cordysean@163.com>
2026-01-09 19:21:27 +0800

    feat: (Undecided) ITheaterSpeakable declaration using System.Speech.Synthesis

---
+++ Foundation/Speaking/ITheaterSpeakable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Speech.Synthesis;
+using System.Text;
+
+namespace AdofaiTheater.Foundation.Speaking
+{
+	// TODO(seanlb): I haven't decided upon how this should work.
+	public interface ITheaterSpeakable
+	{
+		/// <summary>
+		/// Generates the speech and saves it into the file.
+		/// </summary>
+		public Task GenerateSpeech(string fileLocation, PromptBuilder prompt);
+	}
+}
