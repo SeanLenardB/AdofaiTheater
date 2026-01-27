fdf5c32f70d8a8859308f1b760eaed74dbb97a27
SeanLenardB <cordysean@163.com>
2026-01-27 23:50:49 +0800

    feat: hiding and showing theater element by extension, love C# syntax sugar

---
+++ Foundation/Extension/TheaterElementExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdofaiTheater.Foundation.Core;
+
+namespace AdofaiTheater.Foundation.Extension
+{
+    public static class TheaterElementExtension
+    {
+        public static ITheaterElement Hide(this ITheaterElement element)
+        {
+            element.Transform.Visible = false;
+            return element;
+        }
+
+        public static ITheaterElement Show(this ITheaterElement element)
+        {
+            element.Transform.Visible = true;
+            return element;
+        }
+    }
+}
